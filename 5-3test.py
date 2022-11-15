aafe2056ce5bcc2ccefd2297378b785e6929314d
Joshua Thaxton <joshuathaxton540@gmail.com>
2022-11-14 22:42:21 -0800

    Assigment 5

---
+++ 5-3test.py
@@ -0,0 +1,14 @@
+
+
+def shiftN(stringvalue, direction, N):
+    shiftedstring=''
+    if direction == 0:
+
+    return shiftedstring
+
+str = '001100'
+print(rstr)
+print(int(str, 2))
+rstr = shiftN(str, 0, 2)
+print(rstr)
+print(int(str, 2))
