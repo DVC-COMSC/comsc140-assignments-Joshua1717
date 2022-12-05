0e48e7e5bc2513570db1332e2e450e21bb611abe
Joshua Thaxton <joshuathaxton540@gmail.com>
2022-12-04 23:30:49 -0800

    quiz  7 changes

--- Quiz 7.py
+++ Quiz 7.py
@@ -1,52 +1,116 @@
 import xlrd
-from statistics import mean
+from functools import reduce
 
+#exercise 1
 def makeList():
+	path = (r"C:\Users\joshu\Labs\comsc140-assignments-Joshua1717\student.xls")
+	students = xlrd.open_workbook(path)
+	#sheet = students.sheet_by_index(0)
 
-    path = (r"C:\Users\joshu\Labs\comsc140-assignments-Joshua1717\student.xls")
-    students = xlrd.open_workbook(path)
-    sheet = students.sheet_by_index(0)
+	ws = students.sheet_by_index(0)
+	rows = []
 
-    keys = ['id', 'name', 'scores']
-    valueset = [1001, 'Kim', [100, 80, 70, 60]]
-    valueset2 = [1002, 'Bill', [100, 80, 80, 60]]
-    valueset3 = [1003, 'Mary', [90, 80, 70, 100]]
-    valueset4 = [1004, 'Kurt', [100, 100, 100, 90]]
-    valueset5 = [1005, 'Amy', [100, 80, 80, 90]]
+	#print (ws.nrows)
 
-    list = ((dict(zip(keys, valueset,))), (dict(zip(keys, valueset2))), (dict(zip(keys, valueset3)))), (dict(zip(keys, valueset4))), (dict(zip(keys, valueset5)))
+	keys = ws.row_values(0)
+	#print (keys)
 
-    return list
+	students = []
+	for rownumber in range(0, ws.nrows):
+		#print (ws.row_values(rownumber)) 
+		row = ws.row_values(rownumber)
+		row[0], row[1], row[2]
+		students.append(ws.row_values(rownumber))
+	
+	return 	students
 
-    
-def printList(list):
-    print(makeList())
-   
+#exercise 2
+def printList(students):
+	for v in zip(students):
+		print(v)
 
-def scoresbySubject(students):
+#exercise 3	
+def scoresbyStudent(students):
+	
+	scorelist = []
+	for v in range(1,len(students)):
+		scorelist.append(students[v])
+	
+	#print(scorelist)
+	for i in range(0,len(scorelist)):
+		scorelist[i].pop(0)
+	for j in range(0,len(scorelist)):
+		scorelist[j].pop(0)
+		
+	#print(scorelist)
+	#for v in zip(*scorelist):
+		#print(v)
 
-    keys = ['1st', '2nd', '3rd', '4th']
-    score1 = [100, 100, 90,100,100]
-    score2 = [80, 80,80,100, 80]
-    score3 = [70, 80, 70, 100, 80]
-    score4 = [60, 60, 100, 100, 90]
-    
-    scores = ((dict(zip(keys, score1,))), (dict(zip(keys, score2))), (dict(zip(keys, score3))), (dict(zip(keys, score4))))
+	keys = ['1st', '2nd', '3rd', '4th', '5th']
+	scorelist = dict(zip(keys, scorelist))
+	#print(scorelist)
+	
+	return scorelist
 
-    print(scores)
-
-
+#exercise 4
 def findStudents(students):
-    #I can't figure out how to do this one.
-    return result
 
+	scores = []
+	for v in range(1,len(students)):
+		scores.append(students[v])
 
+	for i in range(0,len(scores)):
+		scores[i].pop(0)
+	for j in range(0,len(scores)):
+		scores[j].pop(0)
+
+	sums = []
+	for i in range(0,len(scores)):
+		for j in range(0,3):
+			total = reduce(lambda a, b: a + b, scores[i])
+		sums.append(total)
+	#print(sums)
+	
+	value = []
+	result = filter(lambda x : x > 330, sums)
+	for s in result:
+		value.append(s)
+
+	return value
+
+#exercise 5
 def getAvgList(students):
-    avglist = mean(students.value())
-    return avglist
+	studentscores = []
+	for v in range(1,len(students)):
+		studentscores.append(students[v])
+	
+	for i in range(0,len(studentscores)):
+		studentscores[i].pop(0)
+	for i in range(0,len(studentscores)):
+		studentscores[i].pop(0)
+	#print(studentscores)
 
+	averageScores = []
+	for i in range(0,len(studentscores)):
+		for j in range(0,3):
+			average = reduce(lambda a, b: a + b, studentscores[i]) / 4
+		averageScores.append(average)
+		
+	return averageScores
 
-print(printList(makeList()))
-scoresbySubject(makeList())
-findStudents(makeList())
-print(getAvgList(scoresbySubject(makeList())))
+print(' ')
+print('Make a list of dictionaries from the Excel file ')
+printList(makeList())
+print(' ')
+print('Print the list in a dictionary')
+print(makeList())
+print(' ')
+print('Print the scores by subject ')
+print(scoresbyStudent(makeList()))
+print(' ')
+print('Show scores geater then 330')
+print(getAvgList(makeList()))
+print(' ')
+print('Show average scores for each student ')
+print(findStudents(makeList()))
+print(' ')
